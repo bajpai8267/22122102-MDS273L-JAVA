4a45f14a09ff477404d816a82a4bb0e0fd105dd1
bajpai8267 <aishwaryabajpai419@gmail.com>
2023-03-15 11:55:27 +0530

    Lab5

--- Lab 2/Lab2.java
+++ Lab 2/Lab2.java
@@ -1,82 +1,110 @@
-import java.util.ArrayList;
-import java.util.Scanner;
+// import java.util.ArrayList;
+// import java.util.Scanner;
 
-public class Lab2 {
-  static ArrayList<String> names = new ArrayList<>();
+// public class Lab2 {
+//   static ArrayList<String> names = new ArrayList<>();
 
-  public static void main(String[] args) {
-    Scanner scan = new Scanner(System.in);
-    int option;
+//   public static void main(String[] args) {
+//     Scanner scan = new Scanner(System.in);
+//     int option;
 
-    do {
-      System.out.println("Menu:");
-      System.out.println("1. Enter a name");
-      System.out.println("2. Search for a name");
-      System.out.println("3. Remove a name");
-      System.out.println("4. Show all names");
-      System.out.println("5. Exit");
-      System.out.print("Enter an option: ");
+//     do {
+//       System.out.println("Menu:");
+//       System.out.println("1. Enter a name");
+//       System.out.println("2. Search for a name");
+//       System.out.println("3. Remove a name");
+//       System.out.println("4. Show all names");
+//       System.out.println("5. Exit");
+//       System.out.print("Enter an option: ");
 
-      option = scan.nextInt();
+//       option = scan.nextInt();
 
-      switch (option) {
-        case 1:
-          enterName();
-          break;
-        case 2:
-          searchName();
-          break;
-        case 3:
-          removeName();
-          break;
-        case 4:
-          showAllNames();
-          break;
-        case 5:
-          break;
-        default:
-          System.out.println("Invalid option. Try again.");
-      }
-    } while (option != 5);
+//       switch (option) {
+//         case 1:
+//           enterName();
+//           break;
+//         case 2:
+//           searchName();
+//           break;
+//         case 3:
+//           removeName();
+//           break;
+//         case 4:
+//           showAllNames();
+//           break;
+//         case 5:
+//           break;
+//         default:
+//           System.out.println("Invalid option. Try again.");
+//       }
+//     } while (option != 5);
 
-    scan.close();
-  }
+//     scan.close();
+//   }
 
-  static void enterName() {
-    Scanner scan = new Scanner(System.in);
-    System.out.print("Enter a name: ");
-    String name = scan.nextLine();
-    names.add(name);
-    System.out.println("Name added.");
-  }
+//   static void enterName() {
+//     Scanner scan = new Scanner(System.in);
+//     System.out.print("Enter a name: ");
+//     String name = scan.nextLine();
+//     names.add(name);
+//     System.out.println("Name added.");
+//   }
 
-  static void searchName() {
-    Scanner scan = new Scanner(System.in);
-    System.out.print("Enter a name to search for: ");
-    String name = scan.nextLine();
-    if (names.contains(name)) {
-      System.out.println("Name found.");
-    } else {
-      System.out.println("Name not found.");
+//   static void searchName() {
+//     Scanner scan = new Scanner(System.in);
+//     System.out.print("Enter a name to search for: ");
+//     String name = scan.nextLine();
+//     if (names.contains(name)) {
+//       System.out.println("Name found.");
+//     } else {
+//       System.out.println("Name not found.");
+//     }
+//   }
+
+//   static void removeName() {
+//     Scanner scan = new Scanner(System.in);
+//     System.out.print("Enter a name to remove: ");
+//     String name = scan.nextLine();
+//     if (names.remove(name)) {
+//       System.out.println("Name removed.");
+//     } else {
+//       System.out.println("Name not found.");
+//     }
+//   }
+
+//   static void showAllNames() {
+//     System.out.println("All names:");
+//     for (String name:names) {
+//       System.out.println(name);
+// }
+// }
+// }
+
+
+import java.util.*;
+public class Lab2{
+    public static void main(String[] args){
+        int n;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the numbers you want in your array: ");
+        n= scan.nextInt();
+        int[] array = new int[100];
+        System.out.println("Enter the numbers in array: ");
+        for(int i=0; i<n; i++){
+            array[i] = scan.nextInt();
+        }
+        System.out.println("numbers in the array are: ");
+        for (int i=0; i<n; i++){
+            System.out.println(array[i]);
+            int length = array.length;
+        int sum = 0;
+        for (int  = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+ 
+        double average = sum / length;
+         
+        System.out.println("Average of array : "+average);
+        }
     }
-  }
-
-  static void removeName() {
-    Scanner scan = new Scanner(System.in);
-    System.out.print("Enter a name to remove: ");
-    String name = scan.nextLine();
-    if (names.remove(name)) {
-      System.out.println("Name removed.");
-    } else {
-      System.out.println("Name not found.");
-    }
-  }
-
-  static void showAllNames() {
-    System.out.println("All names:");
-    for (String name:names) {
-      System.out.println(name);
 }
-}
-}
-
---
+++ Lab 5/Lab5.java
@@ -0,0 +1,115 @@
+ import java.util.Scanner;
+
+class Student {
+    String regNo;
+    String name;
+    String email;
+    String phone;
+    String className;
+    String department;
+
+    public Student(String regNo, String name, String email, String phone, String className, String department) {
+        this.regNo = regNo;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.className = className;
+        this.department = department;
+    }
+
+    public void printDetails() {
+        System.out.println("Reg. No.: " + regNo);
+        System.out.println("Name: " + name);
+        System.out.println("Email: " + email);
+        System.out.println("Phone: " + phone);
+        System.out.println("Class: " + className);
+        System.out.println("Department: " + department);
+    }
+}
+
+public class Lab5 {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Student[] students = new Student[100];
+        int studentCount = 0;
+
+        while (true) {
+            System.out.println("\nMenu:");
+            System.out.println("1. Add a student");
+            System.out.println("2. Search for a student by registration number");
+            System.out.println("3. Search for a student by name");
+            System.out.println("4. Display all students");
+            System.out.println("5. Quit");
+
+            System.out.print("Enter your choice: ");
+            String choice = scanner.nextLine();
+
+            if (choice.equals("1")) {
+                System.out.print("Enter registration number: ");
+                String regNo = scanner.nextLine();
+
+                System.out.print("Enter name: ");
+                String name = scanner.nextLine();
+
+                System.out.print("Enter email: ");
+                String email = scanner.nextLine();
+
+                System.out.print("Enter phone: ");
+                String phone = scanner.nextLine();
+
+                System.out.print("Enter class: ");
+                String className = scanner.nextLine();
+
+                System.out.print("Enter department: ");
+                String department = scanner.nextLine();
+
+                Student student = new Student(regNo, name, email, phone, className, department);
+                students[studentCount] = student;
+                studentCount++;
+
+                System.out.println("Student added successfully!");
+            } else if (choice.equals("2")) {
+                System.out.print("Enter registration number: ");
+                String regNo = scanner.nextLine();
+
+                boolean found = false;
+                for (int i = 0; i < studentCount; i++) {
+                    if (students[i].regNo.equals(regNo)) {
+                        students[i].printDetails();
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (!found) {
+                    System.out.println("Student not found!");
+                }
+            } else if (choice.equals("3")) {
+                System.out.print("Enter name: ");
+                String name = scanner.nextLine();
+
+                boolean found = false;
+                for (int i = 0; i < studentCount; i++) {
+                    if (students[i].name.equals(name)) {
+                        students[i].printDetails();
+                        found = true;
+                    }
+                }
+
+                if (!found) {
+                    System.out.println("No more students with this name!");
+                }
+            } else if (choice.equals("4")) {
+                for (int i = 0; i < studentCount; i++) {
+                    students[i].printDetails();
+                }
+            } else if (choice.equals("5")) {
+                break;
+            } else {
+                System.out.println("Invalid choice!");
+            }
+        }
+        scanner.close();
+    }
+}
