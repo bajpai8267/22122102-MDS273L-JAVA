91c827acffd249c7e151ef5073ddaad4fd75d60a
Aishwarya Bajpai <86520419+bajpai8267@users.noreply.github.com>
2023-03-29 11:28:12 +0530

    Update Lab1.java

--- Lab 1/Lab1.java
+++ Lab 1/Lab1.java
@@ -1,11 +1,21 @@
 import java.util.*;
-import java.awt.*;
-public class Lab2 {
-    public static void main(String[] args) {
-        Scanner obj = new Scanner(System.in);
-        System.out.println("Enter Name: ");
-        String Name = obj.nextLine();
-        System.out.println("Your name is: "+StringName);
-    }
-}
+class Lab1 {
+    public static void main(String[] args){
+        String Name;
+        int Age;
+        String Class;
+        char Gender;
+        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter Name,Age,Class");
+        Name = scan.nextLine();
+        Age = Integer.parseInt(scan.nextLine());
+        Class = scan.nextLine();
+        System.out.println("Name :"+Name+"\nAge :"+Age+"\nClass :"+Class);
+        System.out.println("Enter M/F:");
+        Gender = scan.nextLine().charAt(0);
+        if(Gender=='M'){
+            System.out.println("Gender: Male");
+        }else if (Gender=='F'){
+            System.out.println("Gender : Female ");
            
