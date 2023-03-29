5c07a5742569c96ac4b65511cdd54501ca5f8934
bajpai8267 <aishwaryabajpai419@gmail.com>
2023-03-29 11:55:34 +0530

    Lab6

--- Lab6/Lab6.java
+++ Lab6/Lab6.java
@@ -1,217 +1,77 @@
-import java.io.BufferedWriter;
-import java.io.FileWriter;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
 
-
-class Lab6 {
-    private static final int MAX_STUDENTS = 100;
-    private static final Student[] students = new Student[MAX_STUDENTS];
+public class Lab6 {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int choice;
-        do {
-            System.out.println("MENU");
-            System.out.println("1. Add a student");
-            System.out.println("2. Search for a student");
-            System.out.println("3. Update the details of a student");
-            System.out.println("4. Display all students");
-            System.out.println("5. Save student details to file");
-            System.out.println("6. Exit");
-            System.out.print("Enter your choice: ");
-            choice = scanner.nextInt();
-            scanner.nextLine();
-            switch (choice) {
-                case 1:
-                    addStudent(scanner);
-                    break;
-                case 2:
-                    searchStudent(scanner);
-                    break;
-                case 3:
-                    updateStudent(scanner);
-                    break;
-                case 4:
-                    displayAllStudents();
-                    break;
-                case 5:
-                    saveStudentDetailsToFile(scanner);
-                    break;
-                case 6:
-                    System.out.println("Exiting...");
-                    break;
-                default:
-                    System.out.println("Invalid choice, please try again");
-            }
-        } while (choice != 6);
+        String filePath = "text.txt";
+        String fileContent = getFileContent(filePath);
+
+        Map<Character, Integer> vowelsCount = countVowels(fileContent);
+        Map<Character, Integer> digitsCount = countDigits(fileContent);
+        Map<String, Integer> topFiveWords = getTopNWords(fileContent, 5, true);
+        Map<String, Integer> leastFiveWords = getTopNWords(fileContent, 5, false);
+        Map<Character, Integer> topFiveChars = getTopNChars(fileContent, 5, true);
+        Map<Character, Integer> leastFiveChars = getTopNChars(fileContent, 5, false);
+
+        // print or use the results here
     }
 
-    private static void addStudent(Scanner scanner) {
-        int regNo;
-        String name, email, phone, cls, department;
-        System.out.println("Enter the details of the student:");
-        System.out.print("Reg No.: ");
-        regNo = Integer.parseInt(scanner.nextLine());
-        scanner.nextLine(); // consume the newline character
-        System.out.print("Name: ");
-        name = scanner.nextLine();
-        System.out.print("Email: ");
-        email = scanner.nextLine();
-        System.out.print("Phone: ");
-        phone = scanner.nextLine();
-        System.out.print("Class: ");
-        cls = scanner.nextLine();
-        System.out.print("Department: ");
-        department = scanner.nextLine();
-        Student student = new Student(regNo, name, email, phone, cls, department);
-        for (int i = 0; i < MAX_STUDENTS; i++) {
-            if (students[i] == null) {
-                students[i] = student;
-                System.out.println("Student added successfully");
-                break;
+    private static String getFileContent(String filePath) {
+        StringBuilder fileContent = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                fileContent.append(line).append("\n");
             }
-            if (i == MAX_STUDENTS - 1) {
-                System.out.println("Maximum number of students reached, cannot add more students");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return fileContent.toString();
+    }
+
+    private static Map<Character, Integer> countVowels(String fileContent) {
+        Map<Character, Integer> vowelsCount = new HashMap<>();
+        // implementation here
+        return vowelsCount;
+    }
+
+    private static Map<Character, Integer> countDigits(String fileContent) {
+        Map<Character, Integer> digitsCount = new HashMap<>();
+        // implementation here
+        return digitsCount;
+    }
+
+    private static Map<String, Integer> getTopNWords(String fileContent, int n, boolean isDescending) {
+        Map<String, Integer> wordCount = new HashMap<>();
+        // implementation here
+        return wordCount;
+    }
+
+    private static Map<Character, Integer> getTopNChars(String fileContent, int n, boolean isDescending) {
+        Map<Character, Integer> charCount = new HashMap<>();
+        // implementation here
+        return charCount;
+    }
+
+    private static PriorityQueue<Map.Entry<String, Integer>> getTopNEntries(Map<String, Integer> wordCount, int n, boolean isDescending) {
+        PriorityQueue<Map.Entry<String, Integer>> pq = new PriorityQueue<>((a, b) -> {
+            if (isDescending) {
+                return b.getValue() - a.getValue();
+            } else {
+                return a.getValue() - b.getValue();
+            }
+        });
+        for (Map.Entry<String, Integer> entry : wordCount.entrySet()) {
+            pq.offer(entry);
+            if (pq.size() > n) {
+                pq.poll();
             }
         }
-    }
-
-    private static void searchStudent(Scanner scanner) {
-        System.out.println("Enter the search term (name or register number):");
-        String searchTerm = scanner.nextLine();
-        boolean found = false;
-        for (Student student : students) {
-            if (student != null) {
-                if (student.getName().equalsIgnoreCase(searchTerm) || String.valueOf(student.getRegNo()).equals(searchTerm)) {
-                    student.printDetails();
-                    found = true;
-                }
-            }
-        }
-        if (!found) {
-            System.out.println("Student not found");
-        }
-    }
-
-    private static void updateStudent(Scanner scanner) {
-        System.out.println("Enter the search term (name or register number):");
-        String searchTerm = scanner.nextLine();
-        boolean found = false;
-        for (Student student : students) {
-            if (student != null) {
-                if (student.getName().equalsIgnoreCase(searchTerm) || String.valueOf(student.getRegNo()).equals(searchTerm)) {
-                    System.out.println("Enter the new details:");
-                    System.out.print("Name: ");
-                    String name = scanner.nextLine();
-                    System.out.print("Email: ");
-                    String email = scanner.nextLine();
-                    System.out.print("Phone: ");
-                    String phone = scanner.nextLine();
-                    System.out.print("Class: ");
-                    String cls = scanner.nextLine();
-                    System.out.print("Department: ");
-                    String department = scanner.nextLine();
-                    student.setName(name);
-                    student.setEmail(email);
-                    student.setPhone(phone);
-                    student.setCls(cls);
-                    student.setDepartment(department);
-                    System.out.println("Student details updated successfully");
-                    found = true;
-                    break;
-                }
-            }
-        }
-        if (!found) {
-            System.out.println("Student not found");
-        }
-    }
-
-    private static void displayAllStudents() {
-        for (Student student : students) {
-            if (student != null) {
-                student.printDetails();
-            }
-        }
-    }
-
-    private static void saveStudentDetailsToFile(Scanner scanner) {
-        System.out.println("Enter the name of the student to save the details to file:");
-        String studentName = scanner.nextLine();
-        for (Student student : students) {
-            if (student != null) {
-                if (student.getName().equalsIgnoreCase(studentName)) {
-                    String fileName = student.getName() + ".txt";
-                    try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
-                        writer.write(student.toString());
-                        System.out.println("Student details saved to file successfully");
-                    } catch (IOException e) {
-                        System.out.println("Error occurred while saving student details to file: " + e.getMessage());
-                    }
-                    return;
-                }
-            }
-        }
-        System.out.println("Student not found");
-    }
-}
-
-class Student {
-    private int regNo;
-    private String name;
-    private String email;
-    private String phone;
-    private String cls;
-    private String department;
-
-    public Student(int regNo, String name, String email, String phone, String cls, String department) {
-        this.regNo = regNo;
-        this.name = name;
-        this.email = email;
-        this.phone = phone;
-        this.cls = cls;
-        this.department = department;
-    }
-
-    public void printDetails() {
-        System.out.println("Reg No.: " + regNo);
-        System.out.println("Name: " + name);
-        System.out.println("Email: " + email);
-        System.out.println("Phone: " + phone);
-        System.out.println("Class: " + cls);
-        System.out.println("Department: " + department);
-    }
-
-    public String toString() {
-        return regNo + "," + name + "," + email + "," + phone + "," + cls + "," + department;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getRegNo() {
-        return regNo;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public void setCls(String cls) {
-        this.cls = cls;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
+        return pq;
     }
 }
