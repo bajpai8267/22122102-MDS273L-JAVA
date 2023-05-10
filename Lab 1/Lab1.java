f74cd835ec1a9b4a2a6567e3b562a429f883e991
Aishwarya Bajpai <86520419+bajpai8267@users.noreply.github.com>
2023-05-10 23:00:31 +0530

    Update Lab1.java

--- Lab 1/Lab1.java
+++ Lab 1/Lab1.java
@@ -1,21 +1,51 @@
-import java.util.*;
-class Lab1 {
-    public static void main(String[] args){
-        String Name;
-        int Age;
-        String Class;
-        char Gender;
+import java.util.Scanner;
+
+public class Lab1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter Name,Age,Class");
-        Name = scan.nextLine();
-        Age = Integer.parseInt(scan.nextLine());
-        Class = scan.nextLine();
-        System.out.println("Name :"+Name+"\nAge :"+Age+"\nClass :"+Class);
-        System.out.println("Enter M/F:");
-        Gender = scan.nextLine().charAt(0);
-        if(Gender=='M'){
-            System.out.println("Gender: Male");
-        }else if (Gender=='F'){
-            System.out.println("Gender : Female ");
-           
+        // Collect basic details of user
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+        scanner.nextLine(); // consume the newline character left over by nextInt()
+        
+        System.out.print("Enter your gender (m/f): ");
+        char gender = scanner.nextLine().charAt(0);
+        
+        System.out.print("Enter your phone number: ");
+        long phoneNumber = scanner.nextLong();
+        scanner.nextLine(); // consume the newline character left over by nextLong()
+        
+        System.out.print("Enter your email address: ");
+        String email = scanner.nextLine();
+        
+        System.out.print("Enter your state: ");
+        String state = scanner.nextLine();
+        
+        // Display the collected details
+        System.out.println("\nName: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Gender: " + (gender == 'm' ? "MALE" : "FEMALE"));
+        System.out.println("Phone Number: " + phoneNumber);
+        System.out.println("Email Address: " + email);
+        
+        // Check if user is from southern states of India
+        switch (state.toLowerCase()) {
+            case "karnataka":
+            case "kerala":
+            case "tamil nadu":
+            case "andhra pradesh":
+            case "telangana":
+            case "pondicherry":
+            case "lakshadweep":
+            case "andaman and nicobar islands":
+                System.out.println("The Student is from the southern states of India");
+                break;
+            default:
+                break;
+        }
+    }
+}
